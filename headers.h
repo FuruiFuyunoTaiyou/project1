32b21fa43f9bd44f564afc21f0de177f58d475ca
FuruiFuyunoTaiyou <wchen10@stuy.edu>
2017-12-03 22:00:21 -0500

    parsed cmds

--- headers.h
+++ headers.h
@@ -1,2 +1,8 @@
 void print_args(char ** arguments);
 
+char ** parse_cmds(char * input);
+
+char ** parse_args(char * line);
+
+int shell();
+
--- shell.c
+++ shell.c
@@ -4,7 +4,7 @@
 #include <fcntl.h>
 #include <string.h>
 
-void print_args(char ** arguments){ //printing extra space?
+void print_args(char ** arguments){
   int i = 0;
   printf("printing args: [ \n");
   while(arguments[i]){ 
@@ -14,8 +14,31 @@ void print_args(char ** arguments){ //printing extra space?
   printf("] \n");
 }
 
-char ** parse_args(char * line){ //needs to be modified
+char ** parse_cmds(char * input){
+  char * newline = strchr(input, '\n'); //removing works, but warning.
+  if(newline){
+    *newline = NULL;
+  }
+
+  char ** cmds = calloc(6, sizeof(char *));
+  int i = 0;
+  while(i < 5){
+    cmds[i] = strsep(&input, ";"); //didn't work as expected with delim " ; ", but at least spaces are not too detrimental for now.
+    i++;
+  }
+  cmds[i] = NULL;
+  return cmds;
+}
+
+char ** parse_args(char * line){ //needs to be modified for more args?
+  //get rid of the possible \n
   char * s1 = line;
+
+  char * newline = strchr(s1, '\n'); 
+  if(newline){
+    *newline = NULL;
+  }
+
   char ** args = calloc(6, sizeof(char *));;
   int i = 0;
   while(i<5){
@@ -28,10 +51,22 @@ char ** parse_args(char * line){ //needs to be modified
 
 int shell(){
   char input[1000];
-  fgets(input, sizeof(input), stdin);
-  printf("input: %s \n", input);
-  char ** args = parse_args(input);
-  print_args(args);
+  char ** cmds;
+  while(1){
+    printf("type command: ");
+    fgets(input, sizeof(input), stdin);
+    //testing
+    printf("input: %s \n", input);
+    cmds = parse_cmds(input);
+    //testing
+    print_args(cmds);
+  }
+
+  
+  
+  //char ** args = parse_args(input);
+  
+  
   
   return 0;
 }
